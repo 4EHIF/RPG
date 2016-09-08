78239f29e2f9c447eb23db44431863c3e37d19b5
Hedoja <HedojaHD@gmail.com>
2016-09-08 16:03:07 +0200

    Serialization

--- Serialization.py
+++ Serialization.py
@@ -1,9 +1,9 @@
 import MySQLdb
 
 db = MySQLdb.connect(host="localhost",    
-                     user="john",         
-                     passwd="megajonhy",  
-                     db="jonhydb") 
+                     user="",         
+                     passwd="",  
+                     db="") 
 cur = db.cursor()
 cur.execute("SELECT * FROM YOUR_TABLE_NAME")
 
